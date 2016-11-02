8374da53cea3d1e97c896012b9f94965ad50668c
James Eggers <j@jameseggers.com>
2016-11-02 23:44:29 +0000

    code cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -4,24 +4,20 @@ import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString (send, recv)
 import qualified Data.ByteString.Char8 as B8
 
-client' :: Int -> IO ()
-client' = client "localhost"
+client :: IO ()
+client = withSocketsDo $ do
+  localhost <- inet_addr "127.0.0.1"
+  sock <- socket AF_INET Stream defaultProtocol
+  connect sock (SockAddrInet 8000 localhost)
+  sendUserMessage sock
+  sClose sock
 
-client :: String -> Int -> IO ()
-client host port = withSocketsDo $ do
-                addrInfo <- getAddrInfo Nothing (Just host) (Just $ show port)
-                let serverAddr = head addrInfo
-                sock <- socket (addrFamily serverAddr) Stream defaultProtocol
-                connect sock (addrAddress serverAddr)
-                msgSender sock
-                sClose sock
-
-msgSender :: Socket -> IO ()
-msgSender sock = do
+sendUserMessage :: Socket -> IO ()
+sendUserMessage sock = do
   message <- B8.getLine
   send sock $ B8.concat [(B8.pack "GET /echo.php?message="), message, (B8.pack " HTTP/1.1\n\r\n")]
   send sock "Host: localhost\n\r\n"
   send sock "\n\r\n"
-  rMsg <- recv sock 1024
-  B8.putStrLn rMsg
-  msgSender sock
+  receivedMessage <- recv sock 1024
+  B8.putStrLn receivedMessage
+  sendUserMessage sock
